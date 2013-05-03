4861a01d5fefb168e232cca52c9a9df8569fdc0e
Adriano Fernandes <djadrianof@gmail.com>
2013-05-02 23:26:00 -0300

    ...

--- source/javascripts/_app/views/signin.js
+++ source/javascripts/_app/views/signin.js
@@ -6,7 +6,7 @@ SignInView = Backbone.View.extend({
   el : '.box-login',
 
   initialize : function() {
-    _.bindAll( this, 'show_box_login', 'hide_box_login' );
+    _.bindAll( this, 'show_box_login', 'hide_box_login', 'add_login_activity' );
   },
 
   show_box_login : function() {
@@ -17,6 +17,28 @@ SignInView = Backbone.View.extend({
     this.$el.removeClass( 'not-logged' );
   },
 
+  add_login_activity : function() {
+
+    var momentWithoutUrl = {
+      'type'   : 'http://schemas.google.com/AddActivity',
+      'target' : {
+        'id'          : 'app-login',
+        'name'        : 'Signin + Login',
+        'description' : 'The user made successful login in the app',
+        'image'       : 'https://developers.google.com/+/web/snippet/examples/thing.png'
+      }
+    };
+
+    var request = gapi.client.request({
+      'path'   : 'plus/v1/people/me/moments/vault',
+      'method' : 'POST',
+      'body'   : JSON.stringify(momentWithoutUrl)
+    });
+
+    request.execute();
+
+  }
+
   button_callback : function( param ) {
 
     if( param[ 'error' ] ) {
@@ -25,6 +47,7 @@ SignInView = Backbone.View.extend({
 
     if( param[ 'access_token' ] ) {
       window.feed_view = new FeedView();
+      //this.add_login_activity();
     }
 
   }
